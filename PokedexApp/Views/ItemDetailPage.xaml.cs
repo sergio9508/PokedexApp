aea48c2c89d9cc1dd473029c183e9e830765901f
Sergio Gil <sergiog@ordersinseconds.com>
2020-12-14 09:57:48 -0600

    initial commit

---
+++ PokedexApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PokedexApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PokedexApp.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
---
+++ PokedexApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PokedexApp.Models;
+using PokedexApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PokedexApp.Views
+{
+	public partial class NewItemPage : ContentPage
+	{
+		public Item Item { get; set; }
+
+		public NewItemPage()
+		{
+			InitializeComponent();
+			BindingContext = new NewItemViewModel();
+		}
+	}
+}
